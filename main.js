031422c6497f221e4c6e449eafdc32c3069edd60
Haoze Chen <albertchen2017@outlook.com>
2023-06-18 20:49:47 -0700

    Mount app

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const product = 'Socks'
+// Create a Vue app object
+const app = Vue.createApp({
+    // Create a data property that returns the product name
+    data:
+        function () {
+            return {
+            product: 'Socks'
+        }
+    }
+})
+
